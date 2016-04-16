753186d20c5567b8ca9b2d4eb431951333e1fa12
emanriquez <emanriquez@osiristech.cl>
2016-04-16 18:30:33 -0300

    Initial Commit

---
+++ Encriptador/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Encriptador
+//
+//  Created by Osiris on 16-04-16.
+//  Copyright © 2016 Osiris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Encriptador/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Encriptador
+//
+//  Created by Osiris on 16-04-16.
+//  Copyright © 2016 Osiris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
